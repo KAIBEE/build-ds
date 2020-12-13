6cdb289d99c40aefecfc0775091d971bb79964db
Mahamoud SAID OMAR <mahmoud.saidomar@ext.bpifrance.fr>
2020-12-13 01:44:17 +0100

    Configuration sb adding TS and SCSS support

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
@@ -5,6 +7,28 @@ module.exports = {
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+    "@storybook/addon-essentials",
+    "@storybook/addon-a11y"
+  ],
+  "webpackFinal": async (config, { configType }) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "sass-loader"],
+      include: path.resolve(__dirname, "../")
+    });
+    config.module.rules.push({
+      test: /\.ts$/,
+      use: [
+        {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-flow", "@babel/preset-typescript"],
+            plugins: ["@babel/plugin-transform-runtime"]
+          }
+        }
+      ]
+    });
+    config.resolve.extensions.push(".ts");
+    return config;
+  }
+};
