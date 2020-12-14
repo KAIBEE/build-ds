fd56f07534bdfdd9ce47ab048b0e8d661df64ade
Mahamoud SAID OMAR <mahmoud.saidomar@ext.bpifrance.fr>
2020-12-14 01:15:00 +0100

    Refactoring stories

--- stories/Button.stories.ts
+++ stories/Button.stories.ts
@@ -16,6 +16,7 @@ export default {
 const Template = ({ label, ...args }) => {
   // You can either use a function to create DOM elements or use a plain html string!
   // return `<div>${label}</div>`;
+  // @ts-ignore
   return createButton({ label, ...args });
 };
 
